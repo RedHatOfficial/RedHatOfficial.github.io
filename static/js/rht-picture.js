f4dcdc5623c8cdbd1466ffc62497d7096287ceba
Mark Caron <mark.caron@gmail.com>
2025-09-23 10:32:16 -0400

    fix: color-scheme and system prefs improvements

--- static/js/rht-picture.js
+++ static/js/rht-picture.js
@@ -1,20 +1,52 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const body = document.querySelector('body');
+    const body = document.body;
     const pictureEls = document.querySelectorAll('rht-picture');
+
+    let prefersDark = false;
+    const colorScheme = localStorage.getItem('rhdsColorScheme');
+
+    if (!colorScheme || colorScheme === 'light dark') {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        prefersDark = true;
+        pictureEls.forEach((art) => {
+          art.classList.add('dark');
+        });
+      }
+    }
+
+    if (colorScheme === 'dark') {
+      pictureEls.forEach((art) => {
+        art.classList.add('dark');
+      });
+    } else if (colorScheme === 'light') {
+      pictureEls.forEach((art) => {
+        art.classList.remove('dark');
+      });
+    } 
 
     var observer = new MutationObserver(function(mutations) {
       mutations.forEach(function(mutation) {
-        if (mutation.type === "attributes") {
-          let styleAttr = mutation.target.getAttribute('style');
+        if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+          const styleAttr = body.getAttribute('style');
 
-          if (styleAttr.includes('color-scheme: dark')) {
+          if (styleAttr === 'color-scheme: dark;') {
             pictureEls.forEach((art) => {
               art.classList.add('dark');
             });
-          } else {
+          } else if (styleAttr === 'color-scheme: light;') {
             pictureEls.forEach((art) => {
               art.classList.remove('dark');
             });
+          } else {
+            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+              pictureEls.forEach((art) => {
+                art.classList.add('dark');
+              });
+            } else {
+              pictureEls.forEach((art) => {
+                art.classList.remove('dark');
+              });
+            }
           }
         }
       });
