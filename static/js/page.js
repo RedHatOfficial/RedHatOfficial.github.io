822bd1fc032d4b62c530b0d542875ff2f0ea603d
Adam Johnson <adam.b.johnson1@gmail.com>
2025-10-06 12:15:06 -0400

    style: comment spacing / formatting

--- static/js/page.js
+++ static/js/page.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
         'rh-navigation-primary-item[href^="#"], rh-navigation-primary-item > a[href^="#"]'
     );
 
-        // Close dropdowns after clicking an in-page link on mobile viewports
+    // Close dropdowns after clicking an in-page link on mobile viewports
     for (let hashLink of mainNavHashLinks) {
         hashLink.addEventListener('click', function() {
             hashLink.closest('rh-navigation-primary').close();
